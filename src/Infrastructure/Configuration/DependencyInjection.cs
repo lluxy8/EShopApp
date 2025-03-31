dcfea39a20161c466cbd0a01ac140b5671090a2c
muratcan sakin <lluxysa@gmail.com>
2025-03-31 19:32:43 +0300

    Project Created

---
+++ src/Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
