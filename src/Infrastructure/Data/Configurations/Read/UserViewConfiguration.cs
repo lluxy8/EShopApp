41c503422bc4cbb8421df4f48c4b760376894cd4
muratcan sakin <lluxysa@gmail.com>
2025-04-01 14:50:35 +0300

    some cleanup

--- src/Infrastructure/Data/Configurations/Read/UserViewConfiguration.cs
+++ src/Infrastructure/Data/Configurations/Read/UserViewConfiguration.cs
@@ -1,12 +1,6 @@
 ﻿using Core.Common.Constants;
 using Core.Entities.Read;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Data.Configurations.Read
 {
